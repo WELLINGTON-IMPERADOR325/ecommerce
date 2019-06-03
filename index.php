0f0c7c7c30020755c15ece27cbd3be55017ac097
wellington samuel <wellingtonsamuel2@gmail.com>
2019-06-03 17:58:55 -0400

    Autoload das classes do projeto

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php 
-
 require_once("vendor/autoload.php");
-
 $app = new \Slim\Slim();
-
 $app->config('debug', true);
-
 $app->get('/', function() {
     
 $sql = new Hcode\DB\Sql();
@@ -15,5 +11,4 @@ echo json_encode($results);
 });
 
 $app->run();
-
- ?>
+?>
