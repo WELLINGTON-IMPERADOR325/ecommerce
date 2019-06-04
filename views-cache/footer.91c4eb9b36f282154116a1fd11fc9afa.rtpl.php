89d30709af4b81db8d8fa3b637ff29e8aff3d1f3
wellington samuel <wellingtonsamuel2@gmail.com>
2019-06-03 21:49:31 -0400

    classe page e removando arquivos de configuração

---
+++ views-cache/footer.91c4eb9b36f282154116a1fd11fc9afa.rtpl.php
@@ -0,0 +1,3 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>﻿	</body>
+</html>
+	
---
+++ views-cache/header.91c4eb9b36f282154116a1fd11fc9afa.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hcode store</title>
+	</head>
+	<body>
+
+	
