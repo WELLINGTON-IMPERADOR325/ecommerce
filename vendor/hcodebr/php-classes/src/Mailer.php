1521b853aa6f0bf83755a02144bf195e7bb3bb00
wellington samuel <wellingtonsamuel2@gmail.com>
2019-06-11 17:52:18 -0400

    pagina forgot não envia email erro Mailer.php on line 30

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -1,91 +1,82 @@
 <?php
+
 namespace Hcode;
-use Rain\TpL;
-class Mailer {
-	
-	const USERNAME = "manutvgamaaraujo@gmail.com";
-	const PASSWORD = "";
-	const NAME_FROM = "Emnauelle teste ";
-	
-	private $mail;
-	
-	public function __costruct($toAdress,$toName,$subject,$tplname,$data = array())
-	{
-		
-		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"			=> false
-		);
 
-		Tpl::configure( $config );
-		$tpl = new TpL;
-		
-		foreach ($data as $key => $value){
-			$tpl->assign($key, $value);
-		}
-		$html = $tpl->draw($tplName, true);
-		
-		$this->mail = new \PHPMailer;	
-				//$mail = new PHPMailer\PHPMailer\PHPMailer();
+use Rain\Tpl;
 
-		// Diga ao PHPMailer para usar o SMTP
-		$This->$mail->isSMTP();
+class Mailer
+{
+    const USERNAME = "manutvgamaaraujo@gmail.com";
+    const PASSWORD = "anewyerushalem777";
+    const NAME_FROM = "Hcode Store";
+    private $mail;
 
-		// Ativar depuração de SMTP
-		// 0 = off (para uso em produção)
-		// 1 = mensagens do cliente
-		// 2 = mensagens do cliente e do servidor
-		$This->mail->SMTPDebug = 0;
+    public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+    {
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false
+        );
+        Tpl::configure( $config );
+        $tpl = new Tpl;
 
-		$This->mail->Debugoutput = 'html';
+        foreach ($data as $key => $value) {
+            $tpl->assign($key, $value);
+        }
 
-		// Definir o nome do host do servidor de email
-		$This->mail->Host = 'smtp.gmail.com'; //ip do servidor
-		// usar
-		//$mail-> Host = gethostbyname ('smtp.gmail.com');
-		// se sua rede não suporta SMTP sobre IPv6
+        $html = $tpl->draw($tplName, true);
 
-		// Defina o número da porta SMTP - 587 para o TLS autenticado, também conhecido como envio SMTP RFC4409
-		$This->mail->port = 587;// muda-se a porta
-
-		//forma mais simplificada $mail->Host = 'tls: //smtp.gmail.com:587';
-
-		// Definir o sistema de criptografia para usar - ssl (reprovado) ou tls
-		$This->mail->SMTPSecure = 'tls';
-
-		// Se deseja usar a autenticação SMTP
-		$This->mail->SMTPAuth = true;
-
-		// Nome de usuário para usar na autenticação SMTP - use o endereço de e-mail completo para o gmail
-		$This->mail->Username = Mailer::USERNAME;
-
-		// Senha para usar para autenticação SMTP
-		$This->mail->Password = Mailer::PASSWORD;
-
-		// Defina para quem a mensagem deve ser enviada
-		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
-
-		// Definir um endereço de resposta alternativo
-		//$ mail-> addReplyTo ('replyto@example.com ',' Primeiro Último ');
-
-		// Defina para quem a mensagem deve ser enviada
-		$this->mail->addAddress ($toAddress,$toName);
-
-		// Definir a linha de assunto
-		$this->mail->Subject = $subject;
-
-		// Leia um corpo de mensagem HTML de um arquivo externo, converta imagens referenciadas em
-		// converte HTML em um corpo alternativo de texto simples básico
-		$this->mail->msgHTML($html);
-
-		// Substitua o corpo do texto simples por um criado manualmente
-		$this->mail->AltBody = 'Este é um corpo de mensagem de texto simples';
-	}
-		public function send() 
-		{
-			return $this->mail->send();
-		}
+        $this->mail = new \PHPMailer;
+        //Tell PHPMailer to use SMTP
+        $this->mail->isSMTP();
+        $this->mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        //Enable SMTP debugging
+        // 0 = off (for production use)
+        // 1 = client messages
+        // 2 = client and server messages
+        $this->mail->SMTPDebug = 0;
+        //Ask for HTML-friendly debug output
+        $this->mail->Debugoutput = 'html';
+        //Set the hostname of the mail server
+        $this->mail->Host = 'smtp.gmail.com';
+        // use
+        // $this->mail->Host = gethostbyname('smtp.gmail.com');
+        // if your network does not support SMTP over IPv6
+        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+        $this->mail->Port = 587;
+        //Set the encryption system to use - ssl (deprecated) or tls
+        $this->mail->SMTPSecure = 'tls';
+        //Whether to use SMTP authentication
+        $this->mail->SMTPAuth = true;
+        //Username to use for SMTP authentication - use full email address for gmail
+        $this->mail->Username = Mailer::USERNAME;
+        //Password to use for SMTP authentication
+        $this->mail->Password = Mailer::PASSWORD;
+        //Set who the message is to be sent from
+        $this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+        //Set an alternative reply-to address
+        //$this->mail->addReplyTo('replyto@example.com', 'First Last');
+        //Set who the message is to be sent to
+        $this->mail->addAddress($toAddress, $toName);
+        //Set the subject line
+        $this->mail->Subject = $subject;
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        $this->mail->msgHTML($html);
+        //Replace the plain text body with one created manually
+        $this->mail->AltBody = 'This is a plain-text message body';
+        //Attach an image file
+        //$mail->addAttachment('images/phpmailer_mini.png');
+    }
+    public function send()
+    {
+        return $this->mail->send();
+    }
 }
-
-?>
--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -1,48 +1,56 @@
 <?php
+
 namespace Hcode;
 
-use Rain\TpL;
+use Rain\Tpl;
 
-class Page{
-	private $tpl;
-	private $options = [];
-	private $defaults = [
-		"header"=>true,
-		"footer"=>true,
-		"data"=>[]
-	];
-	public function __construct($opts = array(),$tpl_dir = "/views/"){
-		
-		$this->defaults["data"]["session"] = $_SESSION;
-		$this->options = array_merge($this->defaults, $opts);// aqui o metodo __construct vai fazer o merge das paginas
-		
-		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"			=> false
-		);
+class Page 
+{    
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "header" => true,
+        "footer" => true,
+        "data" => []
+    ];
+    
+    public function __construct($opts = array(), $tpl_dir = '/views/')
+    {
+        $this->options = array_merge($this->defaults, $opts);
+        // config
+        $config = array(
+            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . $tpl_dir,
+            "cache_dir"     => $_SERVER['DOCUMENT_ROOT'] . '/views-cache/',
+            "debug"         => false // set to false to improve the speed
+        );
+        
+        Tpl::configure( $config );
 
-		Tpl::configure( $config );
-		$this->tpl = new TpL;
-		$this->setData($this->options["data"]);
-		if($this->options["header"] === true) $this->tpl->draw("header");//cabecalho
-	}
-	private function setData($data = array())
-	{
-		foreach($data as $key => $value){
-			$this->tpl->assign($key,$value);
-		}
-	}
-	public function setTpl($name,$data = array(),$returnHTML = false)//BODY
-	{
-		$this->setData($data);
-		return $this->tpl->draw($name, $returnHTML);
-	}
-	
-	public function __destruct(){//FOOTER
-		if($this->options["footer"] === true ) $this->tpl->draw("footer");
-		
-	}
+        $this->tpl = new Tpl;
+
+        $this->setData($this->options['data']);
+        
+        if($this->options['header'] === true) $this->tpl->draw("header");
+    }
+
+    public function __destruct()
+    {
+        if($this->options['footer'] === true) $this->tpl->draw("footer");
+    }
+
+    private function setData($data = array())
+    {
+        foreach ($data as $key => $value){
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($name, $data = array(), $returnHTML = false)
+    {
+        $this->setData($data);
+
+        return $this->tpl->draw($name, $returnHTML);
+    }
+
+
 }
-
-?>
